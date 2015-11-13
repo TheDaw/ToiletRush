f0a323ebe7d9ed68f2336b58453d4c408b994c9b
James Nolan <th3daw@gmail.com>
2015-11-13 00:28:31 +0000

    Basic game objects started to script logic

---
+++ Assets/Scripts/PersonMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.gameObject.transform.Translate(new Vector3(0, -Time.deltaTime));
+	
+	}
+}
---
+++ Assets/Scripts/PersonSpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonSpawner : MonoBehaviour {
+
+    public int totalActive = 0;
+    public float spawner = 0;
+    public GameObject Man;
+    public GameObject Woman;
+    public GameObject Boy;
+    public GameObject Girl;
+    Random ranGen = new Random();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        spawner = spawner + Time.deltaTime;
+
+        if (spawner > 10)
+        {
+            spawnPerson();
+            spawner = 0;
+            totalActive++;
+        }
+
+        
+	}
+
+    void spawnPerson()
+    {
+        switch (Random.Range(0, 3))
+        {
+            case 0:
+                Instantiate(Man, new Vector3(0, 50, 0), Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(Woman, new Vector3(0, 50, 0), Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(Boy, new Vector3(0, 50, 0), Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(Girl, new Vector3(0, 50, 0), Quaternion.identity);
+                break;
+        }
+    }
+    
+}
