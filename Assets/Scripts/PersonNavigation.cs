a7a0fe204b93c02dae2e7ac488f02e6edd21481e
James Nolan <th3daw@gmail.com>
2015-11-15 19:34:32 +0000

    Scripting taking shape

--- Assets/Scripts/PersonNavigation.cs
+++ Assets/Scripts/PersonNavigation.cs
@@ -5,12 +5,19 @@ public class PersonNavigation : MonoBehaviour {
 
     public enum personStates { moveToQueue, queue, moveToRoom, inRoom, leaving}
     public bool selected = false;
-    personStates state = personStates.moveToQueue;    
+    public personStates state = personStates.moveToQueue;
+    public Vector2 targetPosition;
+    public Vector2 speed = new Vector2((float)0.5, (float)0.5);
+    public Vector2 direction = new Vector2(0, 0);
+    public GameObject queue;
+    
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        queue = GameObject.FindGameObjectWithTag("Queue");
+        Vector2 startPosition = this.transform.position;
+        targetPosition = queue.GetComponent<queueController>().getNextQueuePosition();        
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,14 +25,19 @@ public class PersonNavigation : MonoBehaviour {
         switch (state)
         {
             case personStates.moveToQueue:
+                moveToQueueUpdate();
                 break;
             case personStates.queue:
+                queueUpdate();
                 break;
             case personStates.moveToRoom:
+                //
                 break;
             case personStates.inRoom:
+                inRoomUpdate();
                 break;
             case personStates.leaving:
+                leaveUpdate();
                 break;
         }            
 	
@@ -35,10 +47,30 @@ public class PersonNavigation : MonoBehaviour {
     {
         selected = true;
     }
+    
+
+    void getDirection(Vector2 start, Vector2 end)
+    {
+        direction.x = end.x - start.x;
+        direction.y = end.y - start.y;        
+    }
 
     void moveToQueueUpdate()
     {
+        getDirection(transform.position, targetPosition);
 
+        Vector3 movement = new Vector3(
+       speed.x * direction.x,
+       speed.y * direction.y,
+       0);
+
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+
+        if (transform.position.y <= targetPosition.y)
+        {
+            state = personStates.queue;
+        }    
     }
 
     void queueUpdate()
--- Assets/Scripts/queueController.cs
+++ Assets/Scripts/queueController.cs
@@ -4,14 +4,41 @@ using System.Collections;
 public class queueController : MonoBehaviour {
 
     public bool[] queueSpots = new bool[10];
+    public Vector2[] queuePositions = new Vector2[10];
 
 	// Use this for initialization
 	void Start () {
-	
+
+        float yPos = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            queueSpots[i] = false;
+
+            queuePositions[i].x = 0;
+            queuePositions[i].y = yPos;
+
+            yPos += (float)0.5;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public Vector2 getNextQueuePosition()
+    {
+        int freePos = -1;
+        for (int i = 0; i < 10; i++)
+        {
+            if (queueSpots[i] == false)
+            {
+                freePos = i;
+                queueSpots[i] = true;
+                break;
+            }
+        }
+
+        return queuePositions[freePos];
+    }
 }
